c141068a35aca426a00af963d346b838a6018cd0
Deepakshi725 <126474821+Deepakshi725@users.noreply.github.com>
2025-02-19 09:22:54 +0530

    assignment completed

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,24 @@
 // write product card here
+//eslint-disable-next-line
+import React from "react";
+import ViewProductButton from "../components/button";
+import image from "../assets/product.jpeg";
+import "../components/product.css"; // Importing CSS file
+
+const Productcard = () => {
+  // Static Product Details
+  const productImage = image; 
+  const productName = "Awesome Gadget";
+  const productPrice = "$49.99";
+
+  return (
+    <div className="product-card">
+      <img src={productImage} alt={productName} className="product-image" />
+      <h2 className="product-name">{productName}</h2>
+      <p className="product-price">{productPrice}</p>
+      <ViewProductButton />
+    </div>
+  );
+};
+
+export default Productcard;
